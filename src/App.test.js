3682445c17767d95234a645bcf39d8c0a2ee1a17
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-10-29 08:35:43 -0300

    first test with data-test attribute

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find(`[data-test="component-app"]`);
+  expect(appComponent).toHaveLength(1);
 });
+
+test('renders increment button', () => {});
+
+test('renders counter display', () => {});
+
+test('counter display starts at 0', () => {});
+
+test('clicking button increments counter', () => {});
