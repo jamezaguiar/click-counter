487eb7b68737bf4bf0681894a494ed7a0c03d7dd
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-10-29 09:08:10 -0300

    DRY refactor and completed tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div data-test="component-app"></div>;
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = useCallback(() => {
+    setCounter((prevCounter) => prevCounter + 1);
+  }, []);
+
+  return (
+    <div data-test="component-app">
+      <h1 data-test="counter-display">{counter}</h1>
+      <button data-test="increment-button" onClick={handleIncrement}>
+        Increment
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,49 @@
 import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find(`[data-test="component-app"]`);
-  expect(appComponent).toHaveLength(1);
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function renderComponent
+ * @returns {ShallowWrapper}
+ */
+const renderComponent = () => shallow(<App />);
+
+const findByDataTestAttr = (wrapper, dataTestAttr) =>
+  wrapper.find(`[data-test="${dataTestAttr}"]`);
+
+describe('<App />', () => {
+  test('renders without error', () => {
+    const wrapper = renderComponent();
+    const appComponent = findByDataTestAttr(wrapper, 'component-app');
+    expect(appComponent).toHaveLength(1);
+  });
+
+  test('renders increment button', () => {
+    const wrapper = renderComponent();
+    const button = findByDataTestAttr(wrapper, 'increment-button');
+    expect(button).toHaveLength(1);
+  });
+
+  test('renders counter display', () => {
+    const wrapper = renderComponent();
+    const display = findByDataTestAttr(wrapper, 'counter-display');
+    expect(display).toHaveLength(1);
+  });
+
+  test('counter display starts at 0', () => {
+    const wrapper = renderComponent();
+    const display = findByDataTestAttr(wrapper, 'counter-display');
+    expect(display.text()).toBe('0');
+  });
+
+  test('clicking button increments counter', () => {
+    const wrapper = renderComponent();
+
+    const button = findByDataTestAttr(wrapper, 'increment-button');
+    button.simulate('click');
+
+    const display = findByDataTestAttr(wrapper, 'counter-display');
+
+    expect(display.text()).toBe('1');
+  });
 });
-
-test('renders increment button', () => {});
-
-test('renders counter display', () => {});
-
-test('counter display starts at 0', () => {});
-
-test('clicking button increments counter', () => {});
